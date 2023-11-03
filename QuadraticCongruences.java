c54577a5e6e31589e43ad5ffe759c9f218140937
azoozfzhyg <75800682+azoozfzhyg@users.noreply.github.com>
2023-11-03 20:37:04 +0300

    this is the Barebones of our the project we have to program

---
+++ QuadraticCongruences.java
@@ -0,0 +1,26 @@
+public class QuadraticCongruences{
+// prone to change
+    public static int FindAlphaPrime(int prime, int d){
+        int alpha = 0;
+        return alpha;
+    }
+
+    public static int findInverse(int prime, int a){
+        int inverse = 0;
+        return inverse;
+    }
+
+    public static int[] EcludianAlgorithm(int a, int b){
+        int[] result = new int[2];
+        return result;
+    }
+
+    public static int[] FindSolutions(int prime, int a, int b, int c){
+        int[] solutions = new int[2];
+        return solutions;
+    }
+//
+    public static void main(String[] args) {
+        
+    }
+}
