e7b29afe78a02985483d83809298c0703d1d5b13
azoozfzhyg <75800682+azoozfzhyg@users.noreply.github.com>
2023-11-16 21:47:31 +0300

    did my methods

--- QuadraticCongruences.java
+++ QuadraticCongruences.java
@@ -1,42 +1,56 @@
-public class QuadraticCongruences{
-// prone to change
-    public static int FindAlphaPrime(int prime, int d){
+public class QuadraticCongruences {
+    // prone to change
+    public static int FindAlphaPrime(int prime, int d) {
         int alpha = 0;
         return alpha;
     }
 
-    public static int[] FindSolutions(int prime, int a, int b, int c){
+    public static int[] FindSolutions(int prime, int a, int b, int c) {
         int[] solutions = new int[2];
         return solutions;
     }
 
-    //**// tougher problems
-    public static int ecludianAlgorithm(int a, int b){// Abdulaziz
-        if ( a % b == 0) {
+    // **// tougher problems
+    public static int ecludianAlgorithm(int a, int b) {// Abdulaziz
+        if (a % b == 0) {
             return b;
-        }
-        else {
+        } else {
             return ecludianAlgorithm(b, a % b);
         }
     }
 
-    public static int findInverse(int prime, int b){// Abdulaziz
-        int inverse = 0;
-        return inverse;
-    }
-        // the first index is the coefficient of a, the second is the coefficient of b
-    public static int[] findLinearCombination(int a, int b){
-        int gcd = ecludianAlgorithm(a, b);
-        
+    public static int findInverse(int prime, int b) {// Abdulaziz
+        int factors[] = new int[2];
+        findLinearCombination(prime, b, factors);
+        return factors[1];
     }
 
-    public static int fermatsLittleTheorem(int prime, int d){
+    // the first index is the coefficient of a, the second is the coefficient of b
+    public static void findLinearCombination(int a, int b, int[] factors) {// Abdulaziz
+        if (a % b == 0) {
+            return;
+        }
+        findLinearCombination(b, a % b, factors);
+        int temp = 0;
+        if (factors[0] == 0) {
+            factors[0] = 1;
+            factors[1] = -1 * a / b;
+        } else {
+            temp = factors[0];
+            factors[0] = factors[1];
+            factors[1] = temp + (factors[1] * ((-1) * a / b));
+        }
+        return;
+    }
+
+    public static int fermatsLittleTheorem(int prime, int d) {
         int result = 0;
         return result;
     }
-    //**//
-//
+
+    // **//
+    //
     public static void main(String[] args) {
-        
+
     }
 }
--- Testing.java
+++ Testing.java
@@ -8,7 +8,29 @@ public class Testing {
             return ecludianAlgorithm(b, a % b);
         }
     }
+
+    public static void findLinearCombination(int a, int b, int[] factors){
+        if(a % b == 0 ){
+            return;
+        }
+        findLinearCombination(b, a%b, factors);
+        int temp = 0;
+        if(factors[0] == 0){
+            factors[0] = 1;
+            factors[1] = -1 * a/b;
+        }
+            else{
+                temp = factors[0];
+                factors[0] = factors[1];
+            factors[1] = temp + ( factors[1] * ((-1)*a/b));
+            }
+            return;
+    }
+
     public static void main(String[] args) {
-        System.out.println("the Gcd is: " + ecludianAlgorithm(3021, 510));
+        System.out.println("the Gcd is: " + ecludianAlgorithm(503, 25));
+        int[] factors = new int[2];
+        findLinearCombination(503, 25, factors);
+        System.out.println("the factors are: " + factors[0] + "," + factors[1]+"\nthe inverse is: " + factors[1] + "\n");
     }
 }
