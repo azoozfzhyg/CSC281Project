b74760347902744775d9ef79df20947e098081c0
azoozfzhyg <75800682+azoozfzhyg@users.noreply.github.com>
2023-11-16 21:55:03 +0300

    I'm done with my methods

--- QuadraticCongruences.java
+++ QuadraticCongruences.java
@@ -20,14 +20,28 @@ public class QuadraticCongruences{
         }
     }
 
-    public static int findInverse(int prime, int b){// Abdulaziz
-        int inverse = 0;
-        return inverse;
+    public static int findInverse(int prime, int b) {// Abdulaziz
+        int factors[] = new int[2];
+        findLinearCombination(prime, b, factors);
+        return factors[1];
     }
-        // the first index is the coefficient of a, the second is the coefficient of b
-    public static int[] findLinearCombination(int a, int b){
-        int gcd = ecludianAlgorithm(a, b);
-        
+
+    // the first index is the coefficient of a, the second is the coefficient of b
+    public static void findLinearCombination(int a, int b, int[] factors) {// Abdulaziz
+        if (a % b == 0) {
+            return;
+        }
+        findLinearCombination(b, a % b, factors);
+        int temp = 0;
+        if (factors[0] == 0) {
+            factors[0] = 1;
+            factors[1] = -1 * a / b;
+        } else {
+            temp = factors[0];
+            factors[0] = factors[1];
+            factors[1] = temp + (factors[1] * ((-1) * a / b));
+        }
+        return;
     }
 
     public static int fermatsLittleTheorem(int prime, int d){
