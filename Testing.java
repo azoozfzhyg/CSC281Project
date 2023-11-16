a972a94a0e8fb72790f3e2c2f6e75e9bb257edf3
azoozfzhyg <75800682+azoozfzhyg@users.noreply.github.com>
2023-11-16 19:16:10 +0300

    Update Testing.java

--- Testing.java
+++ Testing.java
@@ -1,14 +1,1 @@
-public class Testing {
-    
-    public static int ecludianAlgorithm(int a, int b){// Abdulaziz
-        if ( a % b == 0) {
-            return b;
-        }
-        else {
-            return ecludianAlgorithm(b, a % b);
-        }
-    }
-    public static void main(String[] args) {
-        System.out.println("the Gcd is: " + ecludianAlgorithm(3021, 510));
-    }
-}
+
