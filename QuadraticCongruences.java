e4836d84f6b96f8a19d77303656fd493dc7a901b
Faris19j <121679246+Faris19j@users.noreply.github.com>
2023-11-20 22:56:49 +0300

    Update QuadraticCongruences.java
    addded the method is prime

--- QuadraticCongruences.java
+++ QuadraticCongruences.java
@@ -47,10 +47,18 @@ public class QuadraticCongruences{
     }
 
     public boolean int isprime (int p){
-        boolean result=false;
-        return result;
+      if (num <= 1) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return true; 
     }
-
+}
+        
     public static int fermatsLittleTheorem(int prime, int d){
         int result = 0;
         return result;
